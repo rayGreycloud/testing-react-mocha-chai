43fb5cc4fee27bd266b8ea8d57f4a5c55b88fcde
rayGreycloud <rayGreycloud@gmail.com>
2016-08-12 14:02:43 -0700

    Add CommentBox component and button spec

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -7,8 +7,8 @@ describe('CommentBox', () => {
     expect(component.find('textarea')).to.exist;
   });
 
-  // it('has a button', () => {
-  //   const component = renderComponent(CommentBox);
-  //
-  // });  
+  it('has a button', () => {
+    const component = renderComponent(CommentBox);
+    expect(component.find('button')).to.exist;
+  });
 });
