152ede9fbe3f5c8921978804184ad6c871668316
rayGreycloud <rayGreycloud@gmail.com>
2016-08-17 13:27:36 -0700

    Start rebuild of test_helper

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,36 +1,12 @@
-import _$ from 'jquery';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TestUtils from 'react-addons-test-utils';
-import jsdom from 'jsdom';
-import chai, { expect } from 'chai';
-import chaiJquery from 'chai-jquery';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from '../src/reducers';
+// Rebuild of test_helper file
 
-global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
-global.window = global.document.defaultView;
-global.navigator = global.window.navigator;
-const $ = _$(window);
+// Set up testing environment
 
-chaiJquery(chai, chai.util, $);
 
-function renderComponent(ComponentClass, props = {}, state = {}) {
-  const componentInstance =  TestUtils.renderIntoDocument(
-    <Provider store={createStore(reducers, state)}>
-      <ComponentClass {...props} />
-    </Provider>
-  );
+// Build 'renderComponent' helper
 
-  return $(ReactDOM.findDOMNode(componentInstance));
-}
 
-$.fn.simulate = function(eventName, value) {
-  if (value) {
-    this.val(value);
-  }
-  TestUtils.Simulate[eventName](this[0]);
-};
+// Build event simulator helper
 
-export {renderComponent, expect};
+
+// Set up chaiJquery
