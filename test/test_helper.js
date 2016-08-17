915baa0d1012c0c0f70ade016219d875ff989e1b
rayGreycloud <rayGreycloud@gmail.com>
2016-08-17 13:40:44 -0700

    Set up testing environment

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,7 +1,13 @@
 // Rebuild of test_helper file
+import jsdom from 'jsdom';
+import jquery from 'jquery';
 
 // Set up testing environment
-
+// Create fake dom for testing
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+global.window = global.document.defaultView;
+// Point to jquery to fake instance of dom
+const $ = jquery(global.window);
 
 // Build 'renderComponent' helper
 
